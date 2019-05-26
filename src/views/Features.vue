61fe16940c1d01ec8ecbc22402244276903e1980
Sabina Niewiadomska <sabina.niewiadomska@inmeta.no>
2019-05-26 21:59:18 +0200

    work started on other features poc

---
+++ src/views/Features.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <h2>Features</h2>
+    <h3>Sound features</h3>
+    <h5>Play white, pink, brown noise</h5>
+    <h5>Few pure tone in the same time</h5>
+    <h5>Few pure tone in the same time where one frequency is louder than another</h5>
+    <h5>Sounds with natural timbre</h5>
+    <h5>Vowel sounds</h5>
+    <h5>Control length of sound</h5>
+    <h5>Control loudness - sound normalization</h5>
+    <h5>Create sound range slider</h5>
+    <h5>Create random cluster</h5>
+    <h3>Measuring features</h3>
+    <h5>Checking the brightness of the screen of participant device</h5>
+    <h5>Checking the luminance from camera (equiluminant conditions)</h5>
+    <h5>Check the size of screen</h5>
+    <h5>Checking is research taken in horizontal mode or vertical mode</h5>
+    <h5>Allowing full screen</h5>
+    <h5>Measuring time from displaying, playing stimuli to submitting answer</h5>
+    <h5>Is participant using speakers or headphones?</h5>
+    <h3>Colour and align-content-space-around features</h3>
+    <h5>Associating loudness (amplitude) of sounds with R,Y,G,B,W color components</h5>
+    <h5>Associating frequency (pitch) of sounds with R,Y,G,B,W color components</h5>
+    <h5>Stevens power law exponent</h5>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
